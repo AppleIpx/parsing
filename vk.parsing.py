d1ab97dfc89fb9b666a4c2191ecd1a2d44da880e
Eugeniy <zhenya.0295@mail.ru>
2022-09-25 23:50:42 +0300

    unneeded library removed

--- vk.parsing.py
+++ vk.parsing.py
@@ -1,6 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 id = str(input())
 access_token = str(input())
 response = requests.get('https://api.vk.com/method/users.get?user_ids=' + id + '&fields=bdate&access_token=' + access_token + '&v=5.131')
-print(response.json())
+res = response.json()
+print(res)
