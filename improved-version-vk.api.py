c39b7ae885f44a08a2b71f04cad7a612ea010f79
Eugeniy <zhenya.0295@mail.ru>
2022-10-05 14:11:58 +0300

    code-improvement

--- improved-version-vk.api.py
+++ improved-version-vk.api.py
@@ -1,17 +1,34 @@
 import requests
 import json
-id = str(input())
-access_token = str(input())
-response = requests.get('https://api.vk.com/method/users.get?user_ids=' + id + '&fields=bdate&access_token=' + access_token + '&v=5.131')
-response = response.json()
+
+def request(id, access_token):
+    https1 = 'https://api.vk.com/method/users.get?user_ids='
+    https2 = '&fields=bdate&access_token='
+    https3 = '&v=5.131'
+    global response
+    response = requests.get(https1 + id + https2 + access_token + https3)
+    response = response.json()
+
+
+request(str(input(id)), str(input('token')))
+
 with open('json-file', 'w') as file:
     json.dump(response, file, indent=3)
 with open('json-file', 'r') as file:
     json_code = json.load(file)
-for i in json_code['response']:
-    print('имя:', i['first_name'], 'фамилия:', i['last_name'], 'id:', i['id'], sep='\n')
-    print()
+for element in json_code['response']:
+    for keys in element:
+        if keys == 'id' or keys == 'first_name' or keys == 'last_name':
+            print(keys + ':', element[keys])
+            id = str(element['id'])
     with open('users', 'a') as file:
-        file.write(i['first_name'])
         file.write('\n')
-        file.write(i['last_name'])
+        file.write('имя: ')
+        file.write(element['first_name'])
+        file.write('\n')
+        file.write('фамилия: ')
+        file.write(element['last_name'])
+        file.write('\n')
+        file.write('id: ')
+        file.write(id)
+        file.write('\n')
