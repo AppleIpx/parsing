1b57c9ff5c6d6e14695a6ed1ee1eb78a233213c5
Evgeniy <113036657+AppleIpx@users.noreply.github.com>
2022-10-24 20:25:45 +0300

    Update improved-version-vk.api.py

--- improved-version-vk.api.py
+++ improved-version-vk.api.py
@@ -6,7 +6,7 @@ def request(id):
     https2 = '&fields=bdate&access_token='
     https3 = '&v=5.131'
     global response
-    access_token = 'vk1.a.yKxxTovFfRF_K3RcLpZWBnO3N62ukrfVwgxAcebEakPJrUXReyvPDEX_z5lTNNwoUwKCPn98qWDGmvOawpialEJPDMQrIjQHE-2FDLqXBRZpRNzIrpCxP8fWpIV3zp26Uv5ncOPt4uwI_ANsItUcTGrnZ2ijCzbPDLL57LdzlNB_8mQ9nYR6dD1Dh90DQ0jZ'
+    access_token = 'access_token'
     response = requests.get(https1 + id + https2 + access_token + https3)
     response = response.json()
 
